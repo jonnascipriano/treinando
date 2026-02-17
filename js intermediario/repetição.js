89199b3547d82724cef1b4f03c40b5045d0e0de9
Jonnas Cipriano <jonnascipriano@gmail.com>
2026-02-16 21:33:07 -0300

    att

---
+++ js intermediario/repetição.js
@@ -0,0 +1,10 @@
+// let execute = true
+
+// while(execute){
+//     let response = window.prompt("Deseja continuar? (s/n)")
+//     if(response === "n"){
+//         execute = false
+//     }
+
+//     console.log("Segue o fluxo...")
+// }
---
+++ js intermediario/repetição2.js
@@ -0,0 +1,9 @@
+// let value = 0
+
+// do{
+//     value++
+//     console.log(value)
+
+// } while(value < 10)
+
+// console.log("Fim do loop")
